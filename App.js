9cf071a754b8d39fcf96a9cb36c1b9309accadc7
Sahil khan <ersahilkhan15@gmail.com>
2023-04-02 12:23:23 +0530

    Episode 02

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-/*
- *   <div id="parent">
- *       <div id="child">
- *           <h1>I am a H1 Tag<h1>
- *           <h2>I am a H2 Tag<h2>
- *       <div>
- *       <div id="child2">
- *           <h1>I am a H1 Tag<h1>
- *           <h2>I am a H2 Tag<h2>
- *       <div>
- *   <div>
- *   ReactElement(object) => HTML (Browser Understands)
- */ 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a H1 Tag"),
+    React.createElement("h1", {}, "I am a Sahil khan"),
     React.createElement("h2", {}, "I am a H2 Tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -22,8 +11,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I am a H2 Tag"),
   ]),
 ]);
-
-//JSX
 
 console.log(parent);
 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React 
+
+# Parcel
+- Dev Build
+- Local Server
+- Tree shaking algorithm - Remove unused code
+- Refreshing the page - (Hot Module Replacement) HMR 
+- It uses the file watching Algorithm (written in C++)
+- Caching things for you - Faster Builds
+- Image optimization
+- Minify file also
+- Bundling 
+- File Compression
+- Consistent Hashing - H.W
+- Code Splitting
+- Differential Bundling - Support older browser
+- Better error handling
+- HTTPS
+- Different build for Dev and production bundles
